8628e3cba8fd657056accc036f91cb70c58f0e24
thej-22 <98275932+thej-22@users.noreply.github.com>
2022-01-26 19:38:24 +0530

    Add files via upload

---
+++ my cap asgn(L)6.py
@@ -0,0 +1,14 @@
+#if it snows
+snows=0
+if snows==1:
+    print("I will go out")
+else:
+    print("i will not go out")
+    
+    
+# if it rains
+rain=1
+if rain==1:
+    print("i will dance")
+    
+
