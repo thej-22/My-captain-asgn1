423b8f9c90340f450694d3026b74d8d9979c1cb2
thej-22 <98275932+thej-22@users.noreply.github.com>
2022-01-26 16:06:11 +0530

    Add files via upload

---
+++ my cap asgn(L)2.py
@@ -0,0 +1,34 @@
+Snacks = ["french fries","burger","waffles"]
+print(Snacks)
+#to add element in the list
+Snacks.append("pizza")
+print(Snacks)
+#copy the elements
+Craving= Snacks.copy()
+print(Snacks)
+#count the times in the list
+a=Snacks.count("burger")
+print(Snacks)
+#extend
+Juice=["coke","sprite","fanta"]
+Snacks.extend(Juice)
+print(Snacks)
+#index
+b= Snacks.index("waffles")
+print(Snacks)
+#insert
+Snacks.insert(1,"cream")
+print(Snacks)
+#pop
+Snacks.pop(1)
+print(Snacks)
+#remove
+Snacks.remove("waffles")
+print(Snacks)
+#sort
+Snacks.sort()
+print(Snacks)
+#clear
+Snacks.clear()
+print(Snacks)
+
