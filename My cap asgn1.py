b5e2b7c1f41292a569fdce079a8e893e48282181
thej-22 <98275932+thej-22@users.noreply.github.com>
2022-01-26 12:35:28 +0530

    Add files via upload

--- My cap asgn1.py
+++ My cap asgn1.py
@@ -3,3 +3,9 @@ Pi=3.14
 area= Pi*R*R
 a=("the area of the circle with the radius {} is : %.4f" %area )
 print(a.format(R))
+
+
+#File name 
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
